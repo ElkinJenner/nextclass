1c4be931be23ee3494ad6e3ad72618b98a99ead4
ElkinJenner <elkinjenner@gmail.com>
2024-05-19 19:41:01 -0500

    New Class

--- src/main/java/com/example/demo/controller/UsuariosController.java
+++ src/main/java/com/example/demo/controller/UsuariosController.java
@@ -10,42 +10,46 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
-@RequestMapping("/")
 public class UsuariosController {
 
     @Autowired
     private UsuariosService usuariosService;
-
-    @GetMapping
-    public List<Usuarios> findAll() {
-        return usuariosService.findAll();
-    }
-
-    @GetMapping("/{id}")
-    public Usuarios findById(@PathVariable Long id) {
-        return usuariosService.findById(id);
-    }
-
-    @PostMapping
-    public void save(@RequestBody Usuarios usuario) {
-        usuariosService.save(usuario);
-    }
-
-    @PutMapping("/{id}")
-    public void update(@PathVariable Long id, @RequestBody Usuarios usuario) {
-        usuario.setIdUsuario(id);
-        usuariosService.update(usuario);
-    }
-
-    @DeleteMapping("/{id}")
-    public void deleteById(@PathVariable Long id) {
-        usuariosService.deleteById(id);
-    }
 
     @GetMapping("/lista")
     public String listarUsuarios(Model model) {
         List<Usuarios> usuarios = usuariosService.findAll();
         model.addAttribute("usuarios", usuarios);
         return "lista";
+    }
+
+    @GetMapping("/usuarios")
+    @ResponseBody
+    public List<Usuarios> findAll() {
+        return usuariosService.findAll();
+    }
+
+    @GetMapping("/usuarios/{id}")
+    @ResponseBody
+    public Usuarios findById(@PathVariable Long id) {
+        return usuariosService.findById(id);
+    }
+
+    @PostMapping("/usuarios")
+    @ResponseBody
+    public void save(@RequestBody Usuarios usuario) {
+        usuariosService.save(usuario);
+    }
+
+    @PutMapping("/usuarios/{id}")
+    @ResponseBody
+    public void update(@PathVariable Long id, @RequestBody Usuarios usuario) {
+        usuario.setIdUsuario(id);
+        usuariosService.update(usuario);
+    }
+
+    @DeleteMapping("/usuarios/{id}")
+    @ResponseBody
+    public void deleteById(@PathVariable Long id) {
+        usuariosService.deleteById(id);
     }
 }
---
+++ src/main/java/com/example/demo/model/Paises.java
@@ -0,0 +1,28 @@
+package com.example.demo.model;
+
+public class Paises {
+    // Atributos
+    private int idPais;
+    private String pais;
+    // Constructores
+    public Paises(){}
+    public Paises(int idPais, String pais) {
+        this.idPais = idPais;
+        this.pais = pais;
+    }
+    // Getters y Setters
+    public int getIdPais() {
+        return idPais;
+    }
+    public void setIdPais(int idPais) {
+        this.idPais = idPais;
+    }
+    public String getPais() {
+        return pais;
+    }
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+    
+
+}
