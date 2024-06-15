4f9285802ca225a5c03417489c1a6ab2509e4494
ElkinJenner <elkinjenner@gmail.com>
2024-06-14 22:06:49 -0500

    New updates

--- src/main/java/com/example/demo/service/LoginService.java
+++ src/main/java/com/example/demo/service/LoginService.java
@@ -2,6 +2,13 @@ package com.example.demo.service;
 
 import com.example.demo.dao.UsuariosDaoImpl;
 import com.example.demo.model.Usuarios;
+import com.example.demo.utils.Validation;
+
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,8 +17,16 @@ public class LoginService {
 
     @Autowired
     private UsuariosDaoImpl usuariosDao;
+    @Autowired
+    HttpSession session;
 
-    public Usuarios validateUser(String email, String password) {
-        return usuariosDao.findByEmailAndPassword(email, password);
+    public Usuarios validateUser(String email, String password)
+            throws NoSuchAlgorithmException, IOException, CloneNotSupportedException {
+        String hashedPassword = Validation.md5(password);
+        Usuarios usuarios = usuariosDao.findByEmailAndPassword(email, hashedPassword);
+        if (usuarios != null) {
+            session.setAttribute("usuarios", usuarios);
+        }
+        return usuarios;
     }
 }
